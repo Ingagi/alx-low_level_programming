eb6b153d995339d1abeda2a68fc9f566d7c23ba4
Ingagi <oshumbusho3@gmail.com>
2023-11-20 01:20:57 -0800

    C - malloc, free

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,24 +1,23 @@
-
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *s;
+    char *s;
 
-	s = _strdup("ALX SE");
-	if (s == NULL)	
-	{
-	printf("failed to allocate memory\n");
-	return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+    s = _strdup("Holberton");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
